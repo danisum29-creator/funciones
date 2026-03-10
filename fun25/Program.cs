5ae31b29876d3cd45ec02b2513fd11b33bb23133
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 23:04:20 -0600

    pri

---
+++ fun25/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace fun25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("ingrese un numero ");
+            n = int.Parse(Console.ReadLine());
+
+            if(EsPrimo(n)==true)
+            {
+                Console.WriteLine("el numero: " + n + " es primo "); 
+            }
+            else
+            {
+                Console.WriteLine("el numero: " + n + " no es primo ");
+            }
+        }
+        static bool EsPrimo(int n)
+        {
+            bool primo = false;
+            for (int i = 2; i < n; i++) { 
+            if(i == 0)
+                    primo = true;
+                break;
+            }
+            return primo;
+        }
+    }
+}
