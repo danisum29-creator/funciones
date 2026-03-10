03c89c1c975e36345acae1d1ecd80ae38bc8c6a8
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 20:39:47 -0600

    funcin

---
+++ fun8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace fun8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("ingrese un numero"); 
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero"); 
+            b = int.Parse(Console.ReadLine());
+
+            
+        }
+
+        static int SumarMostrar(int a, int b)
+        {
+            int suma = 0; 
+            suma = a+ b;
+            return suma;
+        }
+    }
+}
