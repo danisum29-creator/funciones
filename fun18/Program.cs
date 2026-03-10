007c14d70042885abf587a6fd401a9c71842f10c
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 21:55:56 -0600

    arreglo

---
+++ fun18/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace fun18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[2];
+
+            Console.WriteLine("ingrese 3 numeros");
+
+            numeros.Reverse();
+            int menor = numeros[0];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (menor < numeros[i])
+                {
+                    menor = numeros[i];
+                }
+            }
+
+        }
+    }
+}
