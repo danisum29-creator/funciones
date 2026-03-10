608aa76c95f9d4bc21e36cd6e280405940505db2
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 20:01:29 -0600

    mensaje

---
+++ fun2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    
+
+    static void Main(string[] args)
+    {
+        
+
+        MostrarNombre(" ");
+        
+    }
+
+    static void MostrarNombre( string nombre)
+    {
+        
+        Console.WriteLine("ingrese un nombre");
+     nombre = Console.ReadLine();
+        Console.WriteLine("el nombre ingresado es: " + nombre);
+
+        
+    }
+}
+
---
+++ fun3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class program
+{
+    static void main()
+    {
+        MostrarMensaje(); 
+    }
+
+    static void MostrarMensaje()
+    {
+        Console.WriteLine("programacion en C#");
+    }
+
+}
