788819fe64a976a5ef7a260ade614a4fef744d29
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 22:35:04 -0600

    contarvocal

---
+++ fun21/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace fun21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.WriteLine("ingrese un texto");
+            texto = Console.ReadLine();
+        }
+        static int ContarLetras(string texto)
+        {
+            int contvocal = 0;
+            texto.ToLower();
+
+            foreach (char letra in texto)
+            {
+                contvocal++;
+            }
+        }
+    }
+}
