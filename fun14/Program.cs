84d748c6718ba707abd5a53f90b0e6400e88986f
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 21:32:20 -0600

    con error

--- fun14/Program.cs
+++ fun14/Program.cs
@@ -5,25 +5,27 @@
         static void Main(string[] args)
         {
             double a, b;
-            Console.WriteLine("ingrese el primer numero"); 
+            Console.WriteLine("ingrese el primer numero");
             a = double.Parse(Console.ReadLine());
             Console.WriteLine("ingrese el segundo numero");
             b = double.Parse(Console.ReadLine());
-            Dividir(a, b);
-            Console.WriteLine("el resultado de la division es: "+Dividir(a,b));
-        }
-        static double Dividir(double a, double b)
-        {
-            double cociente=0; 
-            if (b==0)
+            if (b == 0)
             {
-                Console.WriteLine("resultado indefinido ");
-
+                Console.WriteLine("resultado indefinido");
             }
             else
             {
-                cociente = a/b;
+                Dividir(a, b);
+                Console.WriteLine("el resultado de la division es: " + Dividir(a, b));
             }
+        }
+        static double Dividir(double a, double b)
+        {
+            
+            double cociente; 
+           
+                cociente = a/b;
+          
             return cociente;
         }
     }
---
+++ fun15/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace fun15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("ingrese un numero");
+            num = int.Parse(Console.ReadLine());
+            EsPar(num);
+
+
+        }
+        static int EsPar(int num)
+        {
+        bool par = false;
+            if ( num%2==0)
+            {
+                par = true;
+            }
+            else
+            {
+                par = false;
+            }
+            return par;
+        }
+    }
+}
