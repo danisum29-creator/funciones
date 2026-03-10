b9756616cc41c169881392960e35e5be6235c9ef
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 22:01:48 -0600

    menores sin repetir

--- fun18/Program.cs
+++ fun18/Program.cs
@@ -4,19 +4,36 @@
     {
         static void Main(string[] args)
         {
-            int[] numeros = new int[2];
+            int a, b, c; 
 
             Console.WriteLine("ingrese 3 numeros");
+           MenorDeTres(a, b, c);
 
-            numeros.Reverse();
-            int menor = numeros[0];
-            for (int i = 0; i < numeros.Length; i++)
+          
+
+        }
+        static int MenorDeTres(int a, int b, int c)
+        {
+            int menor = 0; 
+            if (a == b && a == c)
             {
-                if (menor < numeros[i])
-                {
-                    menor = numeros[i];
-                }
+                Console.WriteLine("los numeros son iguales");
             }
+
+            else if(a<b && a<c)
+            {
+                menor = a; 
+            }
+            else if (b<c&& b<a)
+
+            {
+                menor = b; 
+            }
+            else if(c<a && c<b)
+            {
+                menor = c;
+            }
+
 
         }
     }
