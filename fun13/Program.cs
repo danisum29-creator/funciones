98bcafd0e9e2190e1e1c01358b96539c1637c215
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 21:02:13 -0600

    copiar suma

---
+++ fun13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace fun13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("ingrese el primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero");
+            b = int.Parse(Console.ReadLine());
+            Sumar(a, b);
+            Console.WriteLine("la suma es: " + Sumar(a, b));
+        }
+        static int Sumar(int a, int b)
+        {
+            int suma = a + b;
+            return suma;
+        }
+    }
+}
