f4adc11eef7d854d423e37f3f62929cfacefd28b
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 21:00:37 -0600

    cambiar a resta

--- fun12/Program.cs
+++ fun12/Program.cs
@@ -9,13 +9,13 @@
             a = int.Parse(Console.ReadLine());
             Console.WriteLine("ingrese el segundo numero");
             b = int.Parse(Console.ReadLine());
-            Sumar(a, b);
-            Console.WriteLine("la suma es: " + Sumar(a, b));
+            Restar(a, b);
+            Console.WriteLine("la resta es: " + Restar(a, b));
         }
-        static int Sumar(int a, int b)
+        static int Restar(int a, int b)
         {
-            int suma = a + b;
-            return suma;
+            int resta = a - b;
+            return resta;
         }
     }
 }
