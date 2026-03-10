794aae9873272a9f6dd46e5f85a649a0b41cdcb7
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 20:58:33 -0600

    sumado

---
+++ fun11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace fun11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("ingrese el primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero");
+            b = int.Parse(Console.ReadLine());
+        Sumar(a, b);
+            Console.WriteLine("la suma es: " +Sumar(a, b));
+        }
+
+        static int Sumar(int a, int b)
+        {
+            int suma = a + b;
+            return suma;
+        }
+    }
+}
