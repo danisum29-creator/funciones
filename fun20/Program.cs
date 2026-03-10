ddbc55154a8fa1cec7dfe5a1b961b0d743eaea9d
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 22:20:38 -0600

    contatr

---
+++ fun20/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace fun20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.WriteLine("ingrese una cadena de texto");
+            texto = Console.ReadLine();
+            ContarLetras(texto);
+            Console.WriteLine("la cantidad de caracteres es: " + ContarLetras(texto));
+        }
+        static int ContarLetras(string texto)
+        {
+            int cont = 1;
+            cont = texto.Length;
+            return cont;
+        }
+    }
+}
