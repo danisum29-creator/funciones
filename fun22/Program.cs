e0893e9121546236c26bc2f1b4a320ae30dfcfbd
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 22:52:19 -0600

    imprimir fac

---
+++ fun22/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace fun22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            int veces;
+            Console.WriteLine("ingrese un texto"); 
+            texto= Console.ReadLine();
+            Console.WriteLine("cuantas veces desea repetirlo");
+            veces = int.Parse(Console.ReadLine());
+            Console.WriteLine(Repetir(texto, veces));
+        }
+
+        static string Repetir(string texto, int veces)
+        {
+            
+            for (int i = 1; i < veces; i++)
+            
+                Console.WriteLine(texto); 
+            return texto;
+
+        }
+    }
+}
---
+++ fun24/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace fun24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("ingrese un numero"); 
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("el factorial de: " + n + " es: " + Factorial(n));
+        }
+
+        static int Factorial(int n)
+        {
+            int factorial = 1; 
+            for(int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+
+        }
+    }
+}
