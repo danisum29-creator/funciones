d8609f9f6bcf62ec04af6db374bc928e0bc5e89d
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 20:33:56 -0600

    doble

---
+++ fun7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace fun7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; 
+            Console.WriteLine("ingrese un numero");
+            n = int.Parse(Console.ReadLine());
+           int resultado =  MostrarDoble(n);
+            Console.WriteLine("el resultado es: " + resultado);
+           
+        }
+
+        static int MostrarDoble(int n)
+        {
+            int doble = 2 * n;
+            return doble;
+        }
+    }
+}
