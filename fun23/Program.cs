f9566dcd656744832ccc47be1076d1879d03574b
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 22:58:38 -0600

    sumas

---
+++ fun23/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace fun23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("ingrese un numero"); 
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("la suma de 1 hasta " + n + " es: " + Sumardel1alN(n));
+
+
+        }
+
+        static int Sumardel1alN(int n)
+        {
+            int suma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                suma = suma + i; 
+            }
+            return suma;
+
+        }
+    }
+}
