da7ac7d204d110c9bdca9734dd67a8986686d0d9
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 19:42:56 -0600

    p1

---
+++ fun1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        saludar();
+    }
+
+    static void saludar()
+    {
+        Console.WriteLine("hola, bienvenido");
+    }
+}
