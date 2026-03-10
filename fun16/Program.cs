73961b3db093d523d5bfc3d0eaa9c4c719d7c8c4
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 21:40:25 -0600

    positio

---
+++ fun16/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace fun16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("ingrese un numero ");
+            n = int.Parse(Console.ReadLine());
+            EsPositivo(n);
+            if (EsPositivo(n) == true)
+            {
+                Console.WriteLine("el numero " +n + " es positivo");
+            }
+            else
+            {
+                Console.WriteLine("el numero "+n + " no es positivo")
+            }
+        }
+
+        static bool EsPositivo(int n)
+        {
+            bool positivo = false;
+            if (n >= 1)
+            {
+                positivo = true;
+            }
+            else
+            {
+                positivo= false;
+            }
+            return positivo;
+        }
+    }
+}
